4c8198ccdd53ad598655bb8c24d93548163a29d7
Mikel <mikel@tiralineasestudio.com>
2019-01-30 10:16:31 +0100

    [UPD] Update lumioanalytics/lia-php-client and formatting.

--- templates/lumio_settings.php
+++ templates/lumio_settings.php
@@ -1,50 +1,60 @@
 <div class="wrap">
 
-    <h1><?php echo WLA_NAME; ?>
-        <small><?php echo 'v' . WLA_VERSION; ?></small>
-    </h1>
+	<h1><?php echo WLA_NAME; ?>
+		<small><?php echo 'v' . WLA_VERSION; ?></small>
+	</h1>
 
-    <form method="post" action="">
+	<form method="post" action="">
 
-        <div class="metabox-holder">
+		<div class="metabox-holder">
 
-            <div class="meta-box-sortables ui-sortable">
+			<div class="meta-box-sortables ui-sortable">
 
-                <div id="wla-panel-settings" class="postbox">
+				<div id="wla-panel-settings" class="postbox">
 
-                    <h2><?php esc_html_e('Plugin Settings', 'wla'); ?></h2>
+					<h2><?php esc_html_e( 'Plugin Settings', 'wla' ); ?></h2>
 
-                    <div >
+					<div >
 
-                        <div class="wla-panel-settings">
+						<div class="wla-panel-settings">
 
-                            <table class="widefat">
-                                <tr>
-                                    <th><label for="wla_key"><?php esc_html_e('Lumio Tracking key',
-                                                'wla') ?></label></th>
-                                    <td><input id="wla_key" name="wla_key" type="text" size="40"
-                                               maxlength="40" value="<?php if (isset($this->_key)) {
-                                            echo esc_attr($this->_key);
-                                        } ?>"></td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2" style="text-align:center"><a href="<?php echo $wla_register_url;?>"><?php esc_html_e('Sign Up', 'wla'); ?></a></td>
-                                </tr>
-                            </table>
+							<table class="widefat">
+								<tr>
+									<th><label for="wla_key">
+									<?php
+									esc_html_e(
+										'Lumio Tracking key',
+										'wla'
+									)
+									?>
+												</label></th>
+									<td><input id="wla_key" name="wla_key" type="text" size="40"
+											   maxlength="40" value="
+												<?php
+												if ( isset( $this->_key ) ) {
+													echo esc_attr( $this->_key );
+												}
+												?>
+										"></td>
+								</tr>
+								<tr>
+									<td colspan="2" style="text-align:center"><a href="<?php echo $wla_register_url; ?>"><?php esc_html_e( 'Sign Up', 'wla' ); ?></a></td>
+								</tr>
+							</table>
 
-                        </div>
+						</div>
 
-                        <input type="submit" class="button-primary"
-                               value="<?php esc_attr_e('Save Changes', 'wla'); ?>"/>
+						<input type="submit" class="button-primary"
+							   value="<?php esc_attr_e( 'Save Changes', 'wla' ); ?>"/>
 
-                    </div>
+					</div>
 
-                </div>
+				</div>
 
-            </div>
+			</div>
 
-        </div>
+		</div>
 
-    </form>
+	</form>
 
 </div>
--- wp-lumio-analytics.php
+++ wp-lumio-analytics.php
@@ -7,25 +7,29 @@
   Author: Lumio
   Author URI: http://lumio-analytics.com/
  */
-define('WLA_DIR', __DIR__);
-define('WLA_NAME','Lumio analytics WordPress Plugin');
-define('WLA_VERSION','1.0.0');
+define( 'WLA_DIR', __DIR__ );
+define( 'WLA_NAME', 'Lumio analytics WordPress Plugin' );
+define( 'WLA_VERSION', '1.0.0' );
 
-spl_autoload_register(function ($class) {
-    $base_dir = __DIR__ . '/lib/'; // your classes folder
-    $file     = $base_dir . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
-require_once(__DIR__ . '/vendor/autoload.php');
+spl_autoload_register(
+	function ( $class ) {
+		$base_dir = __DIR__ . '/lib/'; // your classes folder
+		$file     = $base_dir . str_replace( '\\', '/', $class ) . '.php';
+		if ( file_exists( $file ) ) {
+			require $file;
+		}
+	}
+);
+require_once __DIR__ . '/vendor/autoload.php';
 
-register_activation_hook(__FILE__,
-    array("\Lumio\wp_lumio_analytics\Plugin", 'action_activate')
+register_activation_hook(
+	__FILE__,
+	array( '\Lumio\wp_lumio_analytics\Plugin', 'action_activate' )
 );
 
-register_deactivation_hook(__FILE__,
-    array("\Lumio\wp_lumio_analytics\Plugin", 'action_deactivate')
+register_deactivation_hook(
+	__FILE__,
+	array( '\Lumio\wp_lumio_analytics\Plugin', 'action_deactivate' )
 );
 
 \Lumio\wp_lumio_analytics\Plugin::init();
